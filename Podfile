0668f2401a7d09e5475dc4ac727433d6cd20cde3
TAESAN KIM <taronkim220@gmail.com>
2021-07-07 11:58:14 +0900

    Project Setup

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxSwift_MVVM_Note' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RxSwift_MVVM_Note
+
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+  pod 'Action'
+  pod 'NSObject+Rx'
+  pod 'RxDataSources'
+  pod 'RxCoreData'
+
+end
---
+++ ViewController/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RxSwift_MVVM_Note
+//
+//  Created by sdev-mac on 2021/07/07.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
